38f368dbc0647c65f5ab4042ec8e3e35bf30110d
Johnnie Chang <cchan60@ucsc.edu>
2019-06-03 16:13:34 -0700

    2d proj

--- code/results.py
+++ code/results.py
@@ -1,46 +1,83 @@
 import pickle
 from tqdm import tqdm
+from sklearn.manifold import TSNE
+from matplotlib import pyplot as plt
+import time
 
 from analogies import parse_google_analogies
 import verification
 
-anlgtypes = ['capital-common-countries', 'currency', 'city-in-state', 'family',      'gram1-adjective-to-adverb', 'gram2-opposite', 'gram6-nationality-adjective', 'gram8-plural']
-#anlgtypes = ['capital-common-countries']
+def project_2d(words_ours, words_miks):
+    print(words_ours[0])
+    vecs_ours = [model[w] for w in words_ours]
+    vecs_miks = [model[w] for w in words_miks]
+    print('Projecting')
+    start_time = time.time()
+    proj_ours = tsne.fit(vecs_ours)
+    elapsed_time = time.time() - start_time
+    print('Projecting took {:.2f} sec'.format(elapsed_time))
+    proj_miks = tsne.fit(vecs_miks)
 
-model = verification.load_model("GoogleNews-vectors-negative300.bin")
+    xs_ours, ys_ours = proj_ours[:, 0], proj_ours[:, 1]
+    xs_miks, ys_miks = proj_miks[:, 0], proj_miks[:, 1]
+    plt.scatter(xs_ours, ys_ours, color='blue')
+    plt.scatter(xs_miks, ys_miks, color='red')
+    for w, x, y in zip(words_ours, xs_ours, ys_ours):
+        plt.annotate(w, xy=(x, y), xytext=(0,0), textcoords='offset points')
+    for w, x, y in zip(words_miks, xs_miks, ys_miks):
+        plt.annotate(w, xy=(x, y), xytext=(0,0), textcoords='offset points')
 
-for anlgtype in anlgtypes:
-    print(anlgtype)
-    _, anlgs, _ = parse_google_analogies('questions-words.txt', anlgtype=anlgtype)
-    anlgs = anlgs[0]
-    results = pickle.load(open('../data/'+anlgtype+'_similar_vecs.pickle', 'rb'))
-    euc_sum_ours = 0
-    euc_sum_miks = 0
-    cos_sum_ours = 0
-    cos_sum_miks = 0
-    anlg_num = len(anlgs)
-    for i, anlg in enumerate(anlgs):
-        target = anlg[-1]
-        vec_target = model[target]
-        res_ours, res_miks = results[i]
-        vec_ours = model[res_ours[0][0]]
-        vec_miks = model[res_miks[0][0]]
-        cos_ours = verification.cosine_similarity(vec_ours, vec_target)
-        cos_miks = verification.cosine_similarity(vec_miks, vec_target)
-        euc_ours = verification.euclidean_similarity(vec_ours, vec_target)
-        euc_miks = verification.euclidean_similarity(vec_miks, vec_target)
-        cos_sum_ours += cos_ours
-        cos_sum_miks += cos_miks
-        euc_sum_ours += euc_ours
-        euc_sum_miks += euc_miks
-        #print('our cosine similarity: ' + str(cos_ours))
-        #print('Mikolov cosine similarity: ' + str(cos_miks))
-        #print('our euclidean similarity: ' + str(euc_ours))
-        #print('Mikolov euclidean similarity: ' + str(euc_miks))
-    print('our avg cosine similarity:\t\t' + str(cos_sum_ours/anlg_num))
-    print('Mikolov avg cosine similarity:\t\t' + str(cos_sum_miks/anlg_num))
-    print('our avg euclidean similarity:\t\t' + str(euc_sum_ours/anlg_num))
-    print('Mikolov avg euclidean similarity:\t' + str(euc_sum_miks/anlg_num))
-    print()
+    plt.xlim(min(xs_ours.min(), xs_miks.min())+0.00005, max(xs_ours.max(), xs_miks.max())+0.00005)
+    plt.xlim(min(ys_ours.min(), ys_miks.min())+0.00005, max(ys_ours.max(), ys_miks.max())+0.00005)
+    plt.savefig('temp.pdf')
+
+
+if __name__ == '__main__':
+
+    #anlgtypes = ['capital-common-countries', 'currency', 'city-in-state', 'family', 'gram1-adjective-to-adverb', 'gram2-opposite', 'gram6-nationality-adjective', 'gram8-plural']
+    anlgtypes = ['capital-common-countries']
+
+    tsne = TSNE(n_components=2, random_state=0)
+
+    model = verification.load_model("GoogleNews-vectors-negative300.bin")
+
+    for anlgtype in anlgtypes:
+        print(anlgtype)
+        _, anlgs, _ = parse_google_analogies('questions-words.txt', anlgtype=anlgtype)
+        anlgs = anlgs[0]
+        results = pickle.load(open('../data/'+anlgtype+'_similar_vecs.pickle', 'rb'))
+        euc_sum_ours = 0
+        euc_sum_miks = 0
+        cos_sum_ours = 0
+        cos_sum_miks = 0
+        anlg_num = len(anlgs)
+        for i, anlg in enumerate(anlgs):
+            target = anlg[-1]
+            vec_target = model[target]
+            res_ours, res_miks = results[i]
+
+            project_2d(list(zip(*res_ours))[0], list(zip(*res_miks))[0])
+
+            vec_ours = model[res_ours[0][0]]
+            vec_miks = model[res_miks[0][0]]
+            cos_ours = verification.cosine_similarity(vec_ours, vec_target)
+            cos_miks = verification.cosine_similarity(vec_miks, vec_target)
+            euc_ours = verification.euclidean_similarity(vec_ours, vec_target)
+            euc_miks = verification.euclidean_similarity(vec_miks, vec_target)
+            cos_sum_ours += cos_ours
+            cos_sum_miks += cos_miks
+            euc_sum_ours += euc_ours
+            euc_sum_miks += euc_miks
+            #print('our cosine similarity: ' + str(cos_ours))
+            #print('Mikolov cosine similarity: ' + str(cos_miks))
+            #print('our euclidean similarity: ' + str(euc_ours))
+            #print('Mikolov euclidean similarity: ' + str(euc_miks))
+            break
+
+        print('our avg cosine similarity:\t\t' + str(cos_sum_ours/anlg_num))
+        print('Mikolov avg cosine similarity:\t\t' + str(cos_sum_miks/anlg_num))
+        print('our avg euclidean similarity:\t\t' + str(euc_sum_ours/anlg_num))
+        print('Mikolov avg euclidean similarity:\t' + str(euc_sum_miks/anlg_num))
+        print()
 
 
