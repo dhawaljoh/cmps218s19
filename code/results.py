91626ba36ca9b201cf35b3af32270c48ff9ae21b
Johnnie Chang <cchan60@ucsc.edu>
2019-06-03 15:53:20 -0700

    results

--- code/results.py
+++ code/results.py
@@ -4,8 +4,8 @@ from tqdm import tqdm
 from analogies import parse_google_analogies
 import verification
 
-anlgtypes = ['capital-common-countries', 'currency', 'city-in-state', 'family',      'gram1-adjective-to-adverb', 'gram2-opposite', 'gram6-nationality-adjective', 'gram8-    plural']
-anlgtypes = ['capital-common-countries']
+anlgtypes = ['capital-common-countries', 'currency', 'city-in-state', 'family',      'gram1-adjective-to-adverb', 'gram2-opposite', 'gram6-nationality-adjective', 'gram8-plural']
+#anlgtypes = ['capital-common-countries']
 
 model = verification.load_model("GoogleNews-vectors-negative300.bin")
 
@@ -20,7 +20,6 @@ for anlgtype in anlgtypes:
     cos_sum_miks = 0
     anlg_num = len(anlgs)
     for i, anlg in enumerate(anlgs):
-        print(anlg)
         target = anlg[-1]
         vec_target = model[target]
         res_ours, res_miks = results[i]
@@ -38,9 +37,9 @@ for anlgtype in anlgtypes:
         #print('Mikolov cosine similarity: ' + str(cos_miks))
         #print('our euclidean similarity: ' + str(euc_ours))
         #print('Mikolov euclidean similarity: ' + str(euc_miks))
-    print('our avg cosine similarity:\t' + str(cos_sum_ours/anlg_num))
-    print('Mikolov avg cosine similarity:\t' + str(cos_sum_miks/anlg_num))
-    print('our avg euclidean similarity:\t' + str(euc_sum_ours/anlg_num))
+    print('our avg cosine similarity:\t\t' + str(cos_sum_ours/anlg_num))
+    print('Mikolov avg cosine similarity:\t\t' + str(cos_sum_miks/anlg_num))
+    print('our avg euclidean similarity:\t\t' + str(euc_sum_ours/anlg_num))
     print('Mikolov avg euclidean similarity:\t' + str(euc_sum_miks/anlg_num))
     print()
 
