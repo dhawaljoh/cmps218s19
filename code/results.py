a223429559d1c729b4f68efa3e16616e258bf060
Johnnie Chang <cchan60@ucsc.edu>
2019-06-03 18:19:09 -0700

    plot max cosine per analogy type

--- code/results.py
+++ code/results.py
@@ -8,44 +8,37 @@ import time
 from analogies import parse_google_analogies
 import verification
 
-def project_2d(res_ours, res_miks):
-    plt.cla()
+def project_2d(res_ours, res_miks, anlgtype, anlg):
+    """ Plot 2d projection of our prediction and Mikolov's prediction. """
     words_ours, probs_ours = zip(*res_ours)
     words_miks, probs_miks = zip(*res_miks)
-    print(words_ours[0])
     vecs_ours = [model[w] for w in words_ours]
     vecs_miks = [model[w] for w in words_miks]
     proj_ours = tsne.fit_transform(vecs_ours)
     proj_miks = tsne.fit_transform(vecs_miks)
     xs_ours, ys_ours = proj_ours[:, 0], proj_ours[:, 1]
     xs_miks, ys_miks = proj_miks[:, 0], proj_miks[:, 1]
-    #cmap = matplotlib.cm.get_cmap('viridis')
-    #normalize = matplotlib.colors.Normalize(vmin=min(probs_ours), vmax=max(probs_ours))
-    #colors_ours = [cmap(normalize(value)) for value in probs_ours]
-    #colors_miks = [cmap(normalize(value)) for value in probs_miks]
-    #plt.scatter(xs_ours, ys_ours, color='blue', alpha=probs_ours)
-    #plt.scatter(xs_miks, ys_miks, color='red', alpha=probs_miks)
-    #fig, ax = plt.subplots()
-    cm = plt.cm.get_cmap('RdYlBu')
-    sc_ours = plt.scatter(xs_ours, ys_ours, c=probs_ours, vmin=0, vmax=1, cmap=plt.cm.winter)
+    sc_ours = plt.scatter(xs_ours, ys_ours, c=probs_ours, vmin=0, vmax=1, cmap=plt.cm.get_cmap('winter_r'))
     sc_miks = plt.scatter(xs_miks, ys_miks, c=probs_miks, vmin=0, vmax=1, cmap=plt.cm.Reds)
-    plt.colorbar(sc_ours)
-    plt.colorbar(sc_miks)
+    cb_ours = plt.colorbar(sc_ours)
+    cb_miks = plt.colorbar(sc_miks)
     for w, x, y in zip(words_ours, xs_ours, ys_ours):
         plt.annotate(w, xy=(x, y), xytext=(0,0), textcoords='offset points', color='blue')
     for w, x, y in zip(words_miks, xs_miks, ys_miks):
         plt.annotate(w, xy=(x, y), xytext=(0,0), textcoords='offset points', color='red')
-    #cax, _ = matplotlib.colorbar.make_axes(ax)
-    #cbar = matplotlib.colorbar.ColorbarBase(cax, cmap=cmap, norm=normalize)
     plt.xlim(min(xs_ours.min(), xs_miks.min())+0.00005, max(xs_ours.max(), xs_miks.max())+0.00005)
     plt.xlim(min(ys_ours.min(), ys_miks.min())+0.00005, max(ys_ours.max(), ys_miks.max())+0.00005)
-    plt.savefig('temp.pdf')
-
+    a,b,c,d = anlg
+    plt.title(a + ' is to ' + b + ' what ' + c + ' is to ' + d)
+    #plt.savefig(anlgtype + '_maxcos_' + '_'.join(anlg) +'.pdf')
+    plt.savefig(anlgtype + '_maxcos.pdf')
+    cb_ours.remove()
+    cb_miks.remove()
+    plt.cla()
 
 if __name__ == '__main__':
-
-    #anlgtypes = ['capital-common-countries', 'currency', 'city-in-state', 'family', 'gram1-adjective-to-adverb', 'gram2-opposite', 'gram6-nationality-adjective', 'gram8-plural']
-    anlgtypes = ['capital-common-countries']
+    anlgtypes = ['capital-common-countries', 'currency', 'city-in-state', 'family', 'gram1-adjective-to-adverb', 'gram2-opposite', 'gram6-nationality-adjective', 'gram8-plural']
+    #anlgtypes = ['capital-common-countries', 'family']
 
     tsne = TSNE(n_components=2, random_state=0)
 
@@ -61,12 +54,12 @@ if __name__ == '__main__':
         cos_sum_ours = 0
         cos_sum_miks = 0
         anlg_num = len(anlgs)
+        max_cos_ours = 0
         for i, anlg in enumerate(anlgs):
             target = anlg[-1]
             vec_target = model[target]
             res_ours, res_miks = results[i]
-
-            project_2d(res_ours, res_miks)
+            #project_2d(res_ours, res_miks)
 
             vec_ours = model[res_ours[0][0]]
             vec_miks = model[res_miks[0][0]]
@@ -78,11 +71,13 @@ if __name__ == '__main__':
             cos_sum_miks += cos_miks
             euc_sum_ours += euc_ours
             euc_sum_miks += euc_miks
-            #print('our cosine similarity: ' + str(cos_ours))
-            #print('Mikolov cosine similarity: ' + str(cos_miks))
-            #print('our euclidean similarity: ' + str(euc_ours))
-            #print('Mikolov euclidean similarity: ' + str(euc_miks))
-            break
+
+            if cos_ours > max_cos_ours:
+                project_2d(res_ours, res_miks, anlgtype, anlg)
+            max_cos_ours = max(max_cos_ours, cos_ours)
+
+            #if i > 10:
+            #    break
 
         print('our avg cosine similarity:\t\t' + str(cos_sum_ours/anlg_num))
         print('Mikolov avg cosine similarity:\t\t' + str(cos_sum_miks/anlg_num))
